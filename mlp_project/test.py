14f875cae87e99082fdc940d80cac1f201e73249
Wout Piessens <wout.piessens@student.kuleuven.be>
2024-03-21 15:31:53 +0100

    we're getting closer

--- mlp_project/test.py
+++ mlp_project/test.py
@@ -20,15 +20,15 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-game = pyspiel.create_matrix_game([[-1, -4], [0, -3]], [[-1, 0], [-4, -3]])
+game = pyspiel.create_matrix_game([[0, -0.05, 0.25], [0.05, 0, -0.5], [-0.25, 0.5, 0]], [[0, 0.05, -0.25], [-0.05, 0, 0.5], [0.25, -0.5, 0]])
 env = rl_environment.Environment(game)
 nashqlearner0 = MultiagentQLearner(0, 2,
-                                    [env.game.num_distinct_actions()] * 2,
-                                    StackelbergEqSolver())
+                                    [env.game.num_distinct_actions()] * 2, step_size=0.001,
+                                    joint_action_solver=TwoPlayerNashSolver())
 
 nashqlearner1 = MultiagentQLearner(1, 2,
-                                    [env.game.num_distinct_actions()] * 2,
-                                    StackelbergEqSolver())
+                                    [env.game.num_distinct_actions()] * 2, step_size=0.001,
+                                    joint_action_solver=TwoPlayerNashSolver())
 
 
 print("test")
@@ -37,11 +37,11 @@ print("test")
 
 payoff_tensor = utils.game_payoffs_array(env.game)
 print(payoff_tensor)    
-dyn = dynamics.MultiPopulationDynamics(payoff_tensor, dynamics.replicator)
+dyn = dynamics.SinglePopulationDynamics(payoff_tensor, dynamics.replicator)
 x_list = np.array([[1/3, 1/3, 1/3], [0.2, 0.6, 0.2], [0.7, 0.1, 0.2], [0.05, 0.15, 0.8], [0.4, 0.3, 0.2]])
 
 
-ax = plt.subplot(projection="2x2")
+ax = plt.subplot(projection="3x3")
 ax.quiver(dyn)
 
 for i in range(1000):
@@ -55,8 +55,8 @@ for i in range(1000):
     print(probs2)
 
     
-    ax.scatter(probs1[0], 
-               probs2[0], 
+    ax.scatter([(probs1[0], 
+               probs1[1], probs1[2])],
                color="red", linestyle="dashed", linewidth=0.1)
     actions = [
         nashqlearner0.step(time_step, actions).action,
